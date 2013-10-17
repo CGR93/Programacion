236942c6fcca266c8251b7b7e1050fa8811e3db3
usuario <usuario@B301-14.dai.red>
2013-10-17 16:54:44 +0200

    Clase 17/10/13

--- Unidad1/src/Primeros_ejercicios/Ejercicio2_de_otra_forma.java
+++ Unidad1/src/Primeros_ejercicios/Ejercicio2_de_otra_forma.java
@@ -11,19 +11,18 @@ public class Ejercicio2_de_otra_forma {
 		
 		System.out.print("Dame los dos numero: "); 
 		//ir a la consola, poner los dos numeros y pulsar el intro para ejecutarlo
-		//con numeros decimales poner , (4,5)
+		//con numeros decimales poner (4,5)
 		numero1 = teclado.nextFloat(); //esto es para escribir el primer numero
 		numero2 = teclado.nextFloat(); //esto es para escribir el segundo numero
 		
-		suma = (numero1 + numero2);
-		producto = (numero1 * numero2);
+		suma = (numero1 + numero2); // para hacer la suma
+		producto = (numero1 * numero2); // para hacer el producto
 		media = (numero1 + numero2) / 2; //para hacer la media de dos numeros
 		System.out.println("Numero 1:"+ numero1);
 		System.out.println("Numero 2:"+ numero2);
 		System.out.println("La media es:"+ media);
 		System.out.println("La suma es:"+ suma);
 		System.out.println("La producto es:"+ producto);
-	
 		teclado.close(); //para finalizar el teclado y que no de aviso
 	}
 	
