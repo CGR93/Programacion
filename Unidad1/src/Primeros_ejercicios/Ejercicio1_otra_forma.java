b320bfd86378bc200ee58f332cd2ebc2b3f2d520
usuario <usuario@B301-14.dai.red>
2013-10-17 16:49:47 +0200

    Clase 17/10/13

--- Unidad1/src/Primeros_ejercicios/Ejercicio1_otra_forma.java
+++ Unidad1/src/Primeros_ejercicios/Ejercicio1_otra_forma.java
@@ -6,16 +6,14 @@ public class Ejercicio1_otra_forma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String nombre;
-		String mensaje;
-		Scanner input;
+		String nombre ;
+		Scanner input ;
 		
 		input = new Scanner(System.in);
 		System.out.println("¿Como te llamas?");
 		
 		nombre = input.nextLine();
-		mensaje = "Hola,"+ nombre;
-		System.out.println(nombre);
+		System.out.println("Hola, "+ nombre);
 		input.close();
 		
 	}
