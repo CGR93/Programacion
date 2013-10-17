ce1aadec999b6d39f05ec2ce849f0992dc9790cd
usuario <usuario@B301-14.dai.red>
2013-10-17 16:51:51 +0200

    Clase 17/10/13

--- Unidad1/src/Primeros_ejercicios/Ejercicio2_de_otra_forma.java
+++ Unidad1/src/Primeros_ejercicios/Ejercicio2_de_otra_forma.java
@@ -6,7 +6,7 @@ public class Ejercicio2_de_otra_forma {
 
 	public static void main(String[] args) {
 		float numero1, numero2;
-		float media; 
+		float media, suma, producto; 
 		Scanner teclado = new Scanner(System.in); //declarar el scanner pero es necesario importarlo
 		
 		System.out.print("Dame los dos numero: "); 
@@ -15,10 +15,14 @@ public class Ejercicio2_de_otra_forma {
 		numero1 = teclado.nextFloat(); //esto es para escribir el primer numero
 		numero2 = teclado.nextFloat(); //esto es para escribir el segundo numero
 		
+		suma = (numero1 + numero2);
+		producto = (numero1 * numero2);
 		media = (numero1 + numero2) / 2; //para hacer la media de dos numeros
 		System.out.println("Numero 1:"+ numero1);
 		System.out.println("Numero 2:"+ numero2);
 		System.out.println("La media es:"+ media);
+		System.out.println("La suma es:"+ suma);
+		System.out.println("La producto es:"+ producto);
 	
 		teclado.close(); //para finalizar el teclado y que no de aviso
 	}
