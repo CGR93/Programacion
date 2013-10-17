0d6eaf453b84ea229b26cf62220d28c0a5fe7dae
usuario <usuario@B301-14.dai.red>
2013-10-17 16:38:53 +0200

    Clase 17/10/13

--- Unidad1/src/Primeros_ejercicios/Ejercicio3_4.java
+++ Unidad1/src/Primeros_ejercicios/Ejercicio3_4.java
@@ -1,10 +1,26 @@
 package Primeros_ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio3_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		// Calculen el área de un rectángulo (alineado con los ejes x e y) dadas sus coordenadas x1,x2,y1,y2.
+		double x1, x2, y1, y2 ;
+		double altura, base ;
+		double area;
+		
+		Scanner teclado = new Scanner(System.in); //declarar el scanner pero es necesario importarlo
+		System.out.print("Las coordenadas del rectangulo : ");
+		x1 = teclado.nextFloat();
+		x2 = teclado.nextFloat();
+		y1 = teclado.nextFloat();
+		y2 = teclado.nextFloat();
+		altura = y2 - y1;
+		base = x2 - x1;
+		area = (altura * base);
+		System.out.println("El area es :"+ area);
+		teclado.close(); //para finalizar el teclado y que no de aviso
+			}
 
 }
