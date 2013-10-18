f618acd94cf9bd419d56bfe2f1b36e1fa07b4fda
usuario <usuario@B301-14.dai.red>
2013-10-18 16:32:58 +0200

    Clase 17/10/13

--- Unidad1/src/Primeros_ejercicios/Ejercicio3_3.java
+++ Unidad1/src/Primeros_ejercicios/Ejercicio3_3.java
@@ -6,7 +6,7 @@ public class Ejercicio3_3 {
 
 	public static void main(String[] args) {
 		// Calcular el volumen de una esfera dado su radio.
-		float radio;
+		double radio, radio3;
 		double volumen;
 		
 		Scanner teclado = new Scanner(System.in); //declarar el scanner pero es necesario importarlo
@@ -15,9 +15,10 @@ public class Ejercicio3_3 {
 		//ir a la consola, poner la radio y pulsar el intro para ejecutarlo
 		//con numeros decimales poner , (4,5)
 		radio = teclado.nextFloat(); //esto es para escribir el radio de la cicunferencia
-		radio = radio * radio * radio;
-		volumen = (4 * Math.PI * radio ) / 3; // volumen de la circunferencia
+		radio3 = Math.pow(radio, 3);
+		volumen = (4 * Math.PI * radio3) / 3; // volumen de la circunferencia
 		System.out.println("El volumen es: "+ volumen);
+		
 		teclado.close(); //para finalizar el teclado y que no de aviso
 	}
 
