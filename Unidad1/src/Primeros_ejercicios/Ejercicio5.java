99bfb7504b4c05fac7b0336a7b45726f3d698c81
usuario <usuario@B301-14.dai.red>
2013-10-18 17:36:11 +0200

    Clase 18/10/13

--- Unidad1/src/Primeros_ejercicios/Ejercicio5.java
+++ Unidad1/src/Primeros_ejercicios/Ejercicio5.java
@@ -10,19 +10,20 @@ public class Ejercicio5 {
 		// Crea un programa que pida el tamaño de un archivo (Mbytes) y la velocidad de una línea de datos (Mbps), y muestre el tiempo que tardará en transmitirlo usando dicha línea.
 
 		double tamaño, velocidad;
-		double tiempo;
-		
+			
 		Scanner teclado = new Scanner(System.in);
 		
 		System.out.print("Escribe el tamaño del archivo en Mbytes: ");
 		tamaño = teclado.nextDouble(); //nextDouble por la variable
-		System.out.print("Escribe la velocidad de la linea de datos del ordenador: ");
+		System.out.print("Escribe la velocidad de la linea de datos del ordenador en Mbps: ");
 		velocidad = teclado.nextDouble(); // nextDouble por la variable
 		
-		tiempo = (tamaño * 8) / velocidad; // 1Mbytes = 8Mbites; Mbps = Mbites por segundo
-		
-		System.out.print("El tiempo que tarda en transmitir el archivo es: "+ tiempo);
-		
-		teclado.close();
-			}
+		tamaño = tamaño * 1024 * 1024 * 8 ; // = tamaño * Math.pow(2,20) * 8
+		velocidad = velocidad * 1000000;
+				
+		System.out.print("Datos: "+ tamaño + " bits");
+		System.out.print("\nVelocidad: "+ velocidad + " bps");// \n para poner un intro
+		System.out.print("\nTiempo para transmitir: "+ tamaño/velocidad + " segundos"); //\ para introducir un intro
+		teclado.close();		
+	}
 }
