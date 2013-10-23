9a7ffed2084b9e0ee21cdbf5d9e7d58bacaa4ffc
usuario <usuario@B301-14.dai.red>
2013-10-23 20:22:32 +0200

    Clase 23/10/13

---
+++ Unidad1/src/Ejemplos/Ejercicios_de_la_pizarra.java
@@ -0,0 +1,23 @@
+package Ejemplos;
+
+public class Ejercicios_de_la_pizarra {
+
+	public static void main(String[] args) {
+		double x, x1, x2, x3;
+		
+		x = (Math.pow(5, 2)+ 10) * 3;
+		System.out.println("((5^2)+10)*3 = "+ x);
+		
+		x1 = 35.3 - 53 / 10;
+		System.out.println("35.3 - 53 / 10 = "+ x1);
+		
+		x2 = 1 + 5%1.5 * 8 / 2;
+		System.out.println("1 + 5%1.5 * 8 / 2 = "+ x2);
+		
+		x3 = -(-Math.pow(2, 5)+18 / 5);
+		System.out.println("-(-Math.pow(2, 5)+18 / 5) = "+ x3);
+		
+		
+	}
+
+}
