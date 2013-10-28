3a75c286056f624f01a75fc45591cda824d6880c
usuario <usuario@B301-14.dai.red>
2013-10-28 16:37:55 +0100

    Clase 25/10/13

--- Unidad1/src/Ejercicios_graficos/Coche.java
+++ Unidad1/src/Ejercicios_graficos/Coche.java
@@ -10,40 +10,50 @@ import graphics.Rectangle;
 
 public class Coche {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Canvas.getInstance();
                        
-        Rectangle carroceria = new Rectangle(200,400,200,50);
-        carroceria.setColor(Color.BLACK);
-        carroceria.draw();
-        carroceria.fill();
-        
-        Ellipse rueda1 = new Ellipse(350,420,40,50);
-        rueda1.setColor(Color.CYAN);
-        rueda1.draw();
-        rueda1.fill();
-        
-        Ellipse rueda2 = new Ellipse(350,420,40,50);
-        rueda2.setColor(Color.CYAN);
-        rueda2.draw();
-        rueda2.fill();
-        
-        Ellipse rueda3 = new Ellipse(210,420,40,50);
-        rueda3.setColor(Color.CYAN);
-        rueda3.draw();
-        rueda3.fill();
-        
-        Ellipse rueda4 = new Ellipse(210,420,40,50);
-        rueda4.setColor(Color.CYAN);
-        rueda4.draw();
-        rueda4.fill();
-        
-        Line segment = new Line(390, 500, 450, 450);
-        segment.setColor(Color.BLACK);
-        segment.draw();
-        Line segment2 = new Line(391, 501, 451, 451);
-        segment2.setColor(Color.BLACK);
-        segment2.draw();
+       Rectangle carroceria = new Rectangle(500,450,200,50);
+       carroceria.setColor(Color.BLACK);
+       carroceria.draw();
+       carroceria.fill();
+       
+       Rectangle carroceria1 = new Rectangle(555,420,100,40);
+       carroceria1.setColor(Color.BLACK);
+       carroceria1.draw();
+       carroceria1.fill();
+       
+       Rectangle ventana = new Rectangle(560,425,30,25);
+       ventana.setColor(Color.GREEN);
+       ventana.draw();
+       ventana.fill();
+       
+       Rectangle ventana1 = new Rectangle(618,425,30,25);
+       ventana1.setColor(Color.GREEN);
+       ventana1.draw();
+       ventana1.fill();
+       
+       Rectangle luz = new Rectangle(500,455,15,15);
+       luz.setColor(Color.YELLOW);
+       luz.draw();
+       luz.fill();
+       
+       Rectangle luz1 = new Rectangle(685,455,15,15);
+       luz1.setColor(Color.RED);
+       luz1.draw();
+       luz1.fill();
+       
+       Ellipse rueda1 = new Ellipse(510,480,50,50);
+       rueda1.setColor(Color.BLUE);
+       rueda1.draw();
+       rueda1.fill();
+       
+       Ellipse rueda2 = new Ellipse(645,480,50,50);
+       rueda2.setColor(Color.BLUE);
+       rueda2.draw();
+       rueda2.fill();
+       
+       
         
 	}
 
